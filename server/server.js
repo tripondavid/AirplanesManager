d8537841bdad8a77c81e11e2f229494fa59dc7c8
tripondavid <davidtripon3@gmail.com>
2024-04-14 19:31:28 +0300

    Added DDOS prevention and environment variables for DB connection.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const rateLimit = require("express-rate-limit");
 const airplaneRoutes = require("./routes/airplanesRoutes");
 
 const corsOptions = {
@@ -8,6 +9,12 @@ const corsOptions = {
   allowedHeaders: "Content-Type,Authorization",
 };
 
+const limiter = rateLimit({
+  windowMs: 5000,
+  max: 5,
+});
+
+app.use(limiter);
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
